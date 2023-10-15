129ad34840cc3835bfcf61e1e8d1f5bab44d28de
Gerardo Ramos <gerardoere02@gmail.com>
2023-10-15 00:12:31 -0600

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../app.css';
+	import { AppBar, AppShell, Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead"><h1 class="text-2xl font-bold">PokeSvelte</h1></svelte:fragment>
+			<svelte:fragment slot="trail">
+                <Avatar initials="U" width="w-10" background="bg-emerald-200"/>
+            </svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<nav class="list-nav h-screen">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/first-gen">Primera generacion</a></li>
+			</ul>
+		</nav>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/first-gen/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Table, tableMapperValues, type TableSource } from "@skeletonlabs/skeleton";
+
+	const pokemons = [
+		{ number: 1, name: "Bulbasaur", type: "Grass/Poison", weight: 6.9 },
+		{ number: 2, name: "Ivysaur", type: "Grass/Poison", weight: 13.0 },
+		{ number: 3, name: "Venusaur", type: "Grass/Poison", weight: 100.0 },
+		{ number: 4, name: "Charmander", type: "Fire", weight: 8.5 },
+		{ number: 5, name: "Charmeleon", type: "Fire", weight: 19.0 }
+	];
+				
+	const table: TableSource = {
+		head: ["Number",'Name', 'Type', 'Weight'],
+		body: tableMapperValues(pokemons, ['number','name','type','weight']),
+	}
+</script>
+
+<div class="container mx-auto p-8 space-y-8">
+	<Table source={table}></Table>
+</div>
